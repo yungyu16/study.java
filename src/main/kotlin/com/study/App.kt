9a097aea15f9fcd6bc621b9e71a4896fc770b47b
songjialin <songjialin@vcredit.com>
2019-04-01 14:40:41 +0800

    init

---
+++ src/main/kotlin/com/study/App.kt
@@ -0,0 +1,12 @@
+package com.study
+
+/**
+ * Description: ============================
+ *
+ * CreatedDate: 2019/4/1
+ * Author: songjialin
+ */
+
+fun main(args: Array<String>) {
+    print("hello")
+}
