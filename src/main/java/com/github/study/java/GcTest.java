e4b65707f6cbea174dddc1aeb334584c0a7e5a15
songjialin <songjialin@50lion.com>
2020-07-02 15:51:57 +0800

    init

---
+++ src/main/java/com/github/study/java/GcTest.java
@@ -0,0 +1,17 @@
+package com.github.study.java;
+
+import org.jupiter.common.util.Lists;
+
+import java.util.ArrayList;
+
+/**
+ * CreatedDate: 2020/7/2
+ * Author: songjialin
+ */
+public class GcTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("begin");
+        ArrayList<Object> objects = Lists.newArrayList();
+
+    }
+}
