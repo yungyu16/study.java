ba9b828176c5b3311f81800ac4033a18e4fcdc10
songjialin <songjialin@50lion.com>
2020-08-19 12:28:59 +0800

    user agent

--- src/main/java/com/github/study/java/UdpTest2.java
+++ src/main/java/com/github/study/java/UdpTest2.java
@@ -1,11 +1,24 @@
 package com.github.study.java;
 
+import com.wf.captcha.GifCaptcha;
+import eu.bitwalker.useragentutils.UserAgent;
+import generator.RandomUserAgentGenerator;
+
 /**
  * CreatedDate: 2020/7/15
  * Author: songjialin
  */
 public class UdpTest2 {
     public static void main(String[] args) throws Exception {
-
+        UserAgent ua = UserAgent.parseUserAgentString("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Mobile Safari/537.36");
+        System.out.println(ua.getBrowser());
+        System.out.println(ua.getBrowserVersion());
+        System.out.println(ua.getOperatingSystem());
+        System.out.println(ua.getId());
+        System.out.println(ua);
+        GifCaptcha specCaptcha = new GifCaptcha(150, 75, 5);
+        System.out.println(specCaptcha.text());
+        System.out.println(specCaptcha.toBase64());
+        String randomUserAgent = RandomUserAgentGenerator.getNext();
     }
 }
