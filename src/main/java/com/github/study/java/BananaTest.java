56205e950f99cb45167b955fc97fd8a4b9367d5b
songjialin <songjialin@50lion.com>
2020-09-10 09:49:59 +0800

    FeignTest

--- src/main/java/com/github/study/java/BananaTest.java
+++ src/main/java/com/github/study/java/BananaTest.java
@@ -1,6 +1,7 @@
 package com.github.study.java;
 
 import io.leego.banana.BananaUtils;
+import oshi.SystemInfo;
 
 /**
  * CreatedDate: 2020/9/9
@@ -9,5 +10,8 @@ import io.leego.banana.BananaUtils;
 public class BananaTest {
     public static void main(String[] args) {
         System.out.println(BananaUtils.bananaify("Hello, Banana!"));
+        SystemInfo systemInfo = new SystemInfo();
+        System.out.println(systemInfo.getOperatingSystem());
+        System.out.println(systemInfo.getHardware());
     }
 }
