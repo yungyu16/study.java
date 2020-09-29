68c9670da8f5b3b9827f39b6b76fa59b91a3e008
songjialin <songjialin@50lion.com>
2020-09-29 15:58:49 +0800

    groovy

--- src/main/java/com/github/study/java/AhcTest.java
+++ src/main/java/com/github/study/java/AhcTest.java
@@ -1,19 +1,25 @@
 package com.github.study.java;
 
+import okio.Timeout;
 import org.asynchttpclient.Dsl;
 import org.asynchttpclient.Request;
 import org.asynchttpclient.Response;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * CreatedDate: 2020/8/20
  * Author: songjialin
  */
 public class AhcTest {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
         Request build = Dsl.get("https://www.baidu.com").build();
         Response response = Dsl.asyncHttpClient().executeRequest(build).get();
         System.out.println(response.getResponseBody());
+        Timeout timeout = new Timeout();
+        timeout.timeout(5, TimeUnit.SECONDS);
+        timeout.throwIfReached();
     }
 }
